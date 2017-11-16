317049c1b4107c6b93ee0383c8167c44c8fb1400
dcvasquez1 <dcvasquez@outlook.com>
2017-11-16 09:31:40 -0800

    fixed error in python

--- webapp.py
+++ webapp.py
@@ -6,5 +6,17 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('introduction.html')
     
+@app.route("/company")
+def render_page1():
+    return render_template('byCompany.html')
+
+@app.route("/year")
+def render_page2():
+    return render_template('byYear.html')
+
+@app.route("/graph")
+def render_page3():
+    return render_template('graph.html')
+    
 if __name__=="__main__":
     app.run(debug=False, port=54321)
