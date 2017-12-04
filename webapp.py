1cf2f4b2b2f6a642ac96481fb370009e8e4568cd
dcvasquez1 <dcvasquez@outlook.com>
2017-12-04 09:37:56 -0800

    fixed submit

--- webapp.py
+++ webapp.py
@@ -15,9 +15,9 @@ def render_page1():
         cars = json.load(demographicsdata)
     
     reply_list = get_car_options_company(cars)
-    """
+    
     if 'Company' in request.args:        
-        return render_template('byCompany.html', options = reply_list, reply_company = request.args["Company"], fact_dictionary = get_car_facts_company(request.args["Company"]))
+        return render_template('byCompany.html', options = reply_list, """reply_company = request.args["Company"], fact_dictionary = get_car_facts_company(request.args["Company"]))
     """
     return render_template('byCompany.html', options = reply_list)
 
