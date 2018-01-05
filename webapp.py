5bb013e9ee78aca467bc283e43b78bbc020c9612
dcvasquez1 <dcvasquez@outlook.com>
2018-01-05 10:00:00 -0800

    Finished year and company pages

--- webapp.py
+++ webapp.py
@@ -6,14 +6,23 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-"""
+    
     if 'company_request' in request.args:
         with open('static/cars.json') as demographicsdata:
             cars = json.load(demographicsdata)
     
             reply_list = get_car_options_company(cars)
+            
         return render_template( 'byCompany.html' , options = reply_list , reply_company = request.args["company_request"] , fact_dictionary = get_car_facts_company( cars, request.args["company_request"]) )
-"""    
+    
+    elif 'year_request' in request.args:
+        with open('static/cars.json') as demographicsdata:
+            cars = json.load(demographicsdata)
+            
+            reply_list = get_car_options_year(cars)
+            
+        return render_template( 'byYear.html' , options = reply_list , reply_year = request.args["year_request"] , fact_dictionary = get_car_facts_year(cars, request.args["year_request"])   )
+    
     return render_template('introduction.html')
     
 @app.route("/company")
@@ -23,9 +32,6 @@ def render_page1():
         cars = json.load(demographicsdata)
     
     reply_list = get_car_options_company(cars)
-    
-    if 'company_request' in request.args:
-        return render_template( 'byCompany.html' , options = reply_list , reply_company = request.args["company_request"] , fact_dictionary = get_car_facts_company( cars, request.args["company_request"]) )
     
     return render_template('byCompany.html', options = reply_list)
 
@@ -86,21 +92,49 @@ def get_car_facts_company(cars, company):
             avgMPG_City += c["Fuel Information"]["City mph"]
             avgMPG_Highway += c["Fuel Information"]["Highway mpg"]
             avgHorsepower += c["Engine Information"]["Engine Statistics"]["Horsepower"]
+            
             if c["Identification"]["Classification"] == "Automatic transmission":
-                numerAutomatic += 1
+                numberAutomatic += 1
             else:
                 numberManual += 1
                 
-    avgHorsepower = avgHorsepower / carsMade
-    avgMPG_City = avgMPG_City / carsMade
-    avgMPG_Highway = avgMPG_Highway / carsMade
+    avgHorsepower = "%.2f" % (avgHorsepower / carsMade)
+    avgMPG_City = "%.2f" % (avgMPG_City / carsMade)
+    avgMPG_Highway = "%.2f" % (avgMPG_Highway / carsMade)
     
     return {"Number of Cars Made": carsMade , "Number of Manual Transmission Cars Made": numberManual, "Number of Automatic Transmission Cars Made": numberAutomatic, "Average Highway MPG": avgMPG_Highway, "Average City MPG": avgMPG_City, "Average Horsepower": avgHorsepower}
     
-    """
-def get_car_facts_year(cars, year):
     
-    """
+def get_car_facts_year(cars, year):
+    with open('static/cars.json') as demographicsdata:
+        cars = json.load(demographicsdata)
+    
+    carsMade = 0
+    avgMPG_Highway = 0
+    avgMPG_City = 0
+    avgHorsepower = 0
+    numberManual = 0
+    numberAutomatic = 0
+    
+    for c in cars:
+        if str(c["Identification"]["Year"]) == str(year):
+            carsMade += 1
+            avgMPG_City += c["Fuel Information"]["City mph"]
+            avgMPG_Highway += c["Fuel Information"]["Highway mpg"]
+            avgHorsepower += c["Engine Information"]["Engine Statistics"]["Horsepower"]
+            
+            if c["Identification"]["Classification"] == "Automatic transmission":
+                numberAutomatic += 1
+            else:
+                numberManual += 1
+                
+    avgHorsepower = "%.2f" % (avgHorsepower / carsMade)
+    avgMPG_City = "%.2f" % (avgMPG_City / carsMade)
+    avgMPG_Highway = "%.2f" % (avgMPG_Highway / carsMade)
+    
+    return {"Number of Cars Made": carsMade , "Number of Manual Transmission Cars Made": numberManual, "Number of Automatic Transmission Cars Made": numberAutomatic, "Average Highway MPG": avgMPG_Highway, "Average City MPG": avgMPG_City, "Average Horsepower": avgHorsepower}
+    
+    
     
 if __name__=="__main__":
     app.run(debug=True, port=54321)
